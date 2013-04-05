fc9b2d8831231f021aa44ab14a4e069c2d27ee01
Legrand Benjamin <android@legrand.ws>
2013-04-05 17:27:18 +0200

    [Update config]
    Update config from wiko peax sources
    Move things to be able to start build
    Change patch + kernel definition to be able to build kernel

--- patch_sources.sh
+++ patch_sources.sh
@@ -1,12 +1,7 @@
 #! /bin/bash
-#
-# Credits to CMx10 for this script use for the x10
-# Tof37, Scritch007
-#
 
 function apply_patch()
 {
-    pwd
     echo "patch -p1 < $1"
     patch -p1 < $1
 }
@@ -15,9 +10,11 @@ function apply_patch()
         echo "Welcome to CmX10 patchs installer"
     echo
     if find $(pwd)/patches -name "*.patch" >&/dev/null ; then   
+		rom_root=`pwd`
+		rom_root=$rom_root/../../../
+		cd patches
         patch_directory=`pwd`
-        patch_directory=$patch_directory"/patches"
-        file_list=`find $(pwd)/patches -name "*.patch"`
+        file_list=`find $patch_directory -name "*.patch"`
         echo
                 echo "Patchs files found" 
                 for n in $file_list 
@@ -28,28 +25,36 @@ function apply_patch()
                 done
         echo
         #Go to the ROM root folder
-        cd ../../..
+        cd $rom_root
         echo -n "Please enter number of the patch you want to install (all=0, quit=q): "       
         read ans
             if [ "$ans" == "0" ]; then
                 for a in $file_list 
                 do          
+					dir_path=$(dirname $a)
+					dir_path="."${dir_path#$patch_directory}
+					cd $dir_path
                     apply_patch $a
+					cd $rom_root
                 done
             fi
             if [ "$ans" == "q" ]; then
             exit 0
             fi
             if [ "`echo $ans | sed 's/[0-9]*//'`" == "" ] || [ "ans"=="1" ]; then
-            s=${patchArray[$ans]}
-            if [ "$s" == "" ]; then
+            a=${patchArray[$ans]}
+            if [ "$a" == "" ]; then
                 exit 0
                 fi
             else
                 exit 0
             fi  
-            echo $s
-            apply_patch $s
+			dir_path=$(dirname $a)
+			dir_path="."${dir_path#$patch_directory}
+			cd $dir_path
+            apply_patch $a
+			cd $rom_root
+
             
             
     else
